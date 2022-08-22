8dd9d918b3b337d5424712d54ba81729c6e7c2d0
Kra1o5 <kra1o5x@gmail.com>
2022-08-22 20:22:04 +0000

    overlay: Introduce Immersive Navigation
    Based in Hide gesture navbar from Syberia project
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>
    Change-Id: I339b239ed87ab3c493085164aaa5edd786e158bc
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -198,3 +198,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # RRO Overlays
 PRODUCT_PACKAGES += \
     NavigationBarModeGesturalOverlayFS
+
+# Gesture Overlays
+PRODUCT_PACKAGES += \
+  ImmersiveNavigationOverlay
