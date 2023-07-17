13e882e3d4a6584e99e98d0d11e2734febea05bf
El Dainosor <eldainosor@gmail.com>
2023-07-17 15:32:24 -0300

    [DO NOT MERGE]vendor: switch to arrow merge script

--- tools/merge-aosp.sh
+++ tools/merge-aosp.sh
@@ -1,183 +1,122 @@
 #!/usr/bin/env bash
-# Copyright (C) 2016-2019 The Dirty Unicorns Project
-# Copyright (C) 2016 Jacob McSwain
+# Script to merge latest AOSP tag in AOSP-LEGACY source
+# Can be adapted to other AOSP-based ROMs as well
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# After completion, you'll get the following files in the ROM source dir:
+# 	success - repos where merge succeeded
+# 	failed - repos where merge failed
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Also supports auto-pushing of repos where merge succeeded
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# The source directory; this is automatically two folder up because the script
-# is located in vendor/du/scripts. Other ROMs will need to change this. The logic is
-# as follows:
-# 1. Get the absolute path of the script with readlink in case there is a symlink
-#    This script may be symlinked by a manifest so we need to account for that
-# 2. Get the folder containing the script with dirname
-# 3. Move into the folder that is two folder above that one and print it
-WORKING_DIR=$( cd $( dirname $( readlink -f "${BASH_SOURCE[0]}" ) )/../../.. && pwd )
-
-# The tag you want to merge in goes here
-BRANCH=android-${1}
-
-# Manifest branch
-DU_MANIFEST=snippets/bootleggers.xml
-
-# Google source url
-REPO=https://android.googlesource.com/platform/
-
-# This is the array of upstream repos we track
-upstream=()
-
-# This is the array of repos with merge errors
-failed=()
-
-# This is the array of merged repos
-merged=()
-
-# This is the array of repos to blacklist and not merge
-blacklist=('external/google' 'prebuilts/clang/host/linux-x86' 'prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9')
+# Usage: Just run the script in root of ROM source
+#
 
 # Colors
-COLOR_RED='\033[0;31m'
-COLOR_GREEN='\033[0;32m'
-COLOR_BLANK='\033[0m'
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+end=$'\e[0m'
 
-function is_in_blacklist() {
-  for aosp in ${blacklist[@]}
-  do
-    if [ "$aosp" == "$1" ]; then
-      return 0;
-    fi
-  done
-  return 1;
+# Assumes user is running the script in root of source
+ROM_PATH=$(pwd)
+
+echo $ROM_PATH
+# ROM-specific constants
+BRANCH=tirimbino
+REMOTE_NAME=bootleggers
+REPO_XML_PATH="snippets/bootleggers.xml"
+
+# Blacklisted repos - don't try to merge
+blacklist="manifest \
+device/bootleg/sepolicy \
+device/qcom/sepolicy \
+device/qcom/sepolicy-legacy-um \
+device/qcom/sepolicy_vndr \
+device/qcom/sepolicy_vndr-legacy-um \
+external/asus/stitchimage \
+external/colorkt \
+external/faceunlock \
+external/themelib \
+hardware/bootleg/interfaces \
+hardware/bootleg/compat \
+packages/apps/BootlegDumpster \
+packages/apps/FaceUnlockService \
+packages/apps/Jelly \
+packages/apps/ParallelSpace \
+packages/services/OmniJaws \
+packages/wallpapers/ShishufiedWalls \
+vendor/bootleggers \
+vendor/gapps \
+vendor/shishufied \
+vendor/support"
+
+# Get merge tag from user
+read -p "Enter the AOSP tag you want to merge: " TAG
+
+# Set the base URL for all repositories to be pulled from
+AOSP="https://android.googlesource.com"
+
+reset_branch () {
+  git checkout $BRANCH &> /dev/null
+  git fetch $REMOTE_NAME $BRANCH &> /dev/null
+  git reset --hard $REMOTE_NAME/$BRANCH &> /dev/null
 }
 
-function get_repos() {
-  declare -a repos=( $(repo list | cut -d: -f1) )
-  curl --output /tmp/rebase.tmp $REPO --silent # Download the html source of the Android source page
-  # Since their projects are listed, we can grep for them
-  for i in ${repos[@]}
-  do
-    if grep -q "$i" /tmp/rebase.tmp; then # If Google has it and
-      if grep -q "$i" $WORKING_DIR/manifest/$DU_MANIFEST; then # If we have it in our manifest and
-        if grep "$i" $WORKING_DIR/manifest/$DU_MANIFEST | grep -q "remote="; then # If we track our own copy of it
-          if ! is_in_blacklist $i; then # If it's not in our blacklist
-            upstream+=("$i") # Then we need to update it
-          else
-            echo "$i is in blacklist"
-          fi
-        fi
-      fi
-    fi
-  done
-  rm /tmp/rebase.tmp
-}
+# Logic kanged from some similar script
+repos="$(grep "remote=\"$REMOTE_NAME\"" $ROM_PATH/.repo/manifests/$REPO_XML_PATH  | awk '{print $2}' | awk -F '"' '{print $2}')"
 
-function delete_upstream() {
-  for i in ${upstream[@]}
-  do
-    rm -rf $i
-  done
-}
-
-function force_sync() {
-  echo "Repo Syncing..."
-  sleep 10
-  repo sync -c --force-sync >> /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Repo Sync success"
-  else
-    echo "Repo Sync failure"
-    exit 1
-  fi
-}
-
-function merge() {
-  cd $WORKING_DIR/$1
-  git pull $REPO/$1.git -t $BRANCH
-  if [ $? -ne 0 ]; then # If merge failed
-    failed+=($1) # Add to the list
-  fi
-  # Verify the merge commit is there
-  logOutput=$(git log --oneline -n1)
-  if [[ $logOutput == *$BRANCH* ]]; then # If merge commit is there
-    merged+=($1) # Add to the list
-  fi
-}
-
-function build_repo() {
-  cd $WORKING_DIR/build/make
-  git pull $REPO/build.git -t $BRANCH
-  if [ $? -ne 0 ]; then # If merge failed
-    failed+=('build/make') # Add to the list
-  fi
-  # Verify the merge commit is there
-  logOutput=$(git log --oneline -n1)
-  if [[ $logOutput == *$BRANCH* ]]; then # If merge commit is there
-    merged+=('build/make') # Add to the list
-  fi
-}
-
-function print_result() {
-  if [ ${#failed[@]} -eq 0 ]; then
-    echo ""
-    echo "========== "$BRANCH" is merged sucessfully =========="
-    echo "========= Compile and test before pushing to github ========="
-    echo ""
-  else
-    echo -e $COLOR_RED
-    echo -e "These repos have merge errors: \n"
-    for i in ${failed[@]}
-    do
-      echo -e "$i"
-    done
-    echo -e $COLOR_BLANK
-  fi
-  if [ ${#failed[@]} -ne 0 ]; then
-    echo -e $COLOR_GREEN
-    echo -e "These repos have been sucessfully merged: \n"
-    for i in ${merged[@]}
-    do
-      echo -e "$i"
-    done
-    echo -e $COLOR_BLANK
-  fi
-}
-
-# Start working
-cd $WORKING_DIR
-
-# Get the upstream repos we track
-get_repos
-
-echo "================================================"
-echo "          Force Syncing all your repos          "
-echo "         and deleting all upstream repos        "
-echo " This is done so we make sure you're up to date "
-echo "================================================"
-
-delete_upstream
-force_sync
-
-# Merge every repo in upstream
-for i in ${upstream[@]}
-do
-  merge $i
+for files in success failed; do
+    rm $files 2> /dev/null
+    touch $files
 done
 
-# Merge in the build repo last since is got a different
-# manifest path than what's in Google's source
-build_repo
+for REPO in $repos; do
+    if [[ $blacklist =~ $REPO ]]; then
+        echo -e "\n$REPO is in blacklist, skipping"
+    else
+        case $REPO in
+            build/make) repo_url="$AOSP/platform/build" ;;
+            *) repo_url="$AOSP/platform/$REPO" ;; esac
 
-# Go back home
-cd $WORKING_DIR
+        if wget -q --spider $repo_url; then
+            echo -e "$blu \nMerging $REPO $end"
+            cd $ROM_PATH/$REPO
+            reset_branch
+            git fetch -q $repo_url $TAG &> /dev/null
+            if git merge FETCH_HEAD -q -m "Merge tag '$TAG' into $BRANCH" &> /dev/null; then
+                if [[ $(git rev-parse HEAD) != $(git rev-parse $REMOTE_NAME/$BRANCH) ]] && [[ $(git diff HEAD $REMOTE_NAME/$BRANCH) ]]; then
+                    echo "$REPO" >> $ROM_PATH/success
+                    echo "${grn}Merging $REPO succeeded :) $end"
+                else
+                    echo "$REPO - unchanged"
+                    git reset --hard $REMOTE_NAME/$BRANCH &> /dev/null
+                fi
+            else
+                echo "$REPO" >> $ROM_PATH/failed
+                echo "${red}$REPO merging failed :( $end"
+            fi
+            cd $ROM_PATH
+        fi
+    fi
+done
 
-# Print any repos that failed, so we can fix merge issues
-print_result
+echo -e "$red \nThese repos failed merging: \n $end"
+cat failed
+echo -e "$grn \nThese repos succeeded merging: \n $end"
+cat success
+
+echo $red
+read -p "Do you want to push the succesfully merged repos? (Y/N): " PUSH
+echo $end
+
+if [[ $PUSH == "Y" ]] || [[ $PUSH == "y" ]]; then
+    # Push succesfully merged repos
+    for REPO in $(cat success); do
+        cd $REPO
+        echo -e "Pushing $REPO ..."
+        git push -q $REMOTE_NAME HEAD:$BRANCH &> /dev/null
+        cd $ROM_PATH
+    done
+fi
+
+echo -e "\n${blu}All done :) $end"
