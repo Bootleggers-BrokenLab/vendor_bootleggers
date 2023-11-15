9b528a3bd9e173f34595e3ab745019d734fb51d2
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Switch to AOSPA's face unlock implementation
    Change-Id: I11a13349aadf63820542674761b2216a27ed387f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    -----
    
    overlays: create FaceUnlockOverlay
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I772542d10e0a88f001d7ce166b760d95de67c470
    
    -----
    
    vendor: overlay: Add face unlock overlays
    
    - Custom face enroll activity
    - Modified face enroll education lottie animation from Pixel
    - Face enroll configuration from Pixel
    
    Change-Id: I0daea264fa4364cafaa2f3165c29b3bf9b806209
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Co-authored-by: Sourajit Karmakar <sourajitk@statixos.com>
    
    -----
    
    fixup! vendor: overlay: Add face unlock overlays
    
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: I7e67c77bfa39ea8d7e13efaa8b7883984b99d410
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: Chris Crump <chriscrawford893@gmail.com>
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Co-authored-by: Sourajit Karmakar <sourajitk@statixos.com>
    Co-authored-by: Mesquita <mesquita@aospa.co>

--- config/common.mk
+++ config/common.mk
@@ -24,6 +24,23 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+PRODUCT_PACKAGES += \
+    FaceUnlockOverlay
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
